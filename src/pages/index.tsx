d043a8459fc22179cf71d7e48565d56b04c8ab21
Vagner Silveira de Oliveira <vagner.olliver@yahoo.com.br>
2021-07-21 23:14:17 -0300

    chore: create page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 import Head from 'next/head'
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>My page title</title>
-    </Head>
-    <h1>Hello World!</h1>
+      <Head>
+        <title>My page title</title>
+      </Head>
+      
+      <main className={styles.homeContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+            Get access to the publications <br />
+            <span>for $9,90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   )
 }
