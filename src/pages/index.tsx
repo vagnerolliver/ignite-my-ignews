3fdf5d22432f3b56daf17c1b70a748727128e82f
Vagner Silveira de Oliveira <vagner.olliver@yahoo.com.br>
2021-07-23 07:49:14 -0300

    chore: config static site generator

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import { SubscribeButton } from '../components/SubscribeButton';
@@ -14,6 +14,7 @@ interface HomeProps {
 }
 
 export default function Home({ product }: HomeProps) {
+  console.log(product.amount)
   return (
     <>
       <Head>
@@ -38,7 +39,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JGKN7LDCRFNguZrflmZfTwz', {
     expand: ['product']
   })
@@ -49,12 +50,13 @@ export const getServerSideProps: GetServerSideProps = async () => {
     amount: new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD'
-    }).format(price.unit_amount / 100,)
+    }).format(price.unit_amount / 100)
   }
 
   return { 
      props: {
       product
-     }
+     },
+     revalidate: 60 * 60 * 24
   }
 }
